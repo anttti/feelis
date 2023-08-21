2936c393569b91281f3c3d5a317cb6e798a0d386
Antti Mattila <antti.mattila@futurice.com>
2023-08-21 22:04:13 +0300

    Add pubsub

--- lib/feelis_web/live/present_live/index.ex
+++ lib/feelis_web/live/present_live/index.ex
@@ -2,10 +2,32 @@ defmodule FeelisWeb.PresentLive.Index do
   use FeelisWeb, :live_view
 
   alias Feelis.Presentations
+  alias Feelis.Presence
+  alias Feelis.PubSub
+
+  @presence "poll:presence"
+  @page_turn_topic "topic:page_turn"
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, session, socket) do
+    if connected?(socket) do
+      id = session["user_id"]
+
+      if id do
+        IO.puts("Got existing ID: #{id}")
+
+        {:ok, _} =
+          Presence.track(self(), @presence, id, %{
+            name: "User #{id}",
+            joined_at: :os.system_time(:seconds)
+          })
+      end
+
+      Phoenix.PubSub.subscribe(PubSub, @presence)
+      FeelisWeb.Endpoint.subscribe(@page_turn_topic)
+    end
+
+    {:ok, socket |> assign(:users, %{}) |> handle_joins(Presence.list(@presence))}
   end
 
   @impl true
@@ -21,14 +43,53 @@ defmodule FeelisWeb.PresentLive.Index do
   end
 
   @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff", payload: diff}, socket) do
+    {
+      :noreply,
+      socket
+      |> handle_leaves(diff.leaves)
+      |> handle_joins(diff.joins)
+    }
+  end
+
+  @impl true
+  def handle_info(%{topic: @page_turn_topic, payload: state}, socket) do
+    IO.puts("THIS IS THE EVENT")
+    IO.inspect(state)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("new-user-id", %{"id" => id}, socket) do
+    IO.puts("Got new ID: #{id}")
+
+    {:ok, _} =
+      Presence.track(self(), @presence, id, %{
+        name: "User #{id}",
+        joined_at: :os.system_time(:seconds)
+      })
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def handle_event("next_slide", _params, socket) do
     next_index = socket.assigns.current_index + 1
 
     if Enum.count(socket.assigns.presentation.slides) > next_index do
+      next_slide = Enum.at(socket.assigns.presentation.slides, next_index)
+
+      # Broadcasts the event to ALL OTHER SUBSCRIBERS THAN THE ONE THAT SENDS IT
+      FeelisWeb.Endpoint.broadcast_from(self(), @page_turn_topic, "set_slide", %{
+        id: next_slide.id,
+        title: next_slide.title,
+        description: next_slide.description
+      })
+
       {:noreply,
        socket
        |> assign(:current_index, next_index)
-       |> assign(:current_slide, Enum.at(socket.assigns.presentation.slides, next_index))}
+       |> assign(:current_slide, next_slide)}
     else
       {:noreply, socket}
     end
@@ -39,6 +100,15 @@ defmodule FeelisWeb.PresentLive.Index do
     next_index = socket.assigns.current_index - 1
 
     if next_index >= 0 do
+      next_slide = Enum.at(socket.assigns.presentation.slides, next_index)
+
+      # Broadcasts the event to ALL OTHER SUBSCRIBERS THAN THE ONE THAT SENDS IT
+      FeelisWeb.Endpoint.broadcast_from(self(), @page_turn_topic, "set_slide", %{
+        id: next_slide.id,
+        title: next_slide.title,
+        description: next_slide.description
+      })
+
       {:noreply,
        socket
        |> assign(:current_index, next_index)
@@ -46,5 +116,17 @@ defmodule FeelisWeb.PresentLive.Index do
     else
       {:noreply, socket}
     end
+  end
+
+  defp handle_joins(socket, joins) do
+    Enum.reduce(joins, socket, fn {user, %{metas: [meta | _]}}, socket ->
+      assign(socket, :users, Map.put(socket.assigns.users, user, meta))
+    end)
+  end
+
+  defp handle_leaves(socket, leaves) do
+    Enum.reduce(leaves, socket, fn {user, _}, socket ->
+      assign(socket, :users, Map.delete(socket.assigns.users, user))
+    end)
   end
 end
