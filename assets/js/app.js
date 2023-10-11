59c7efeab48d714056887b0c3da3ba6fc4116476
Antti Mattila <antti.mattila@futurice.com>
2023-10-11 21:44:47 +0300

    Remove adhoc D3

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,66 +36,15 @@ hooks.setSession = {
   },
 };
 
-const drawCloud = (answers) => {
-  const margin = { top: 10, right: 10, bottom: 10, left: 10 },
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+// hooks.drawWordCloud = {
+//   mounted() {
+//     drawCloud(answers);
+//   },
+// };
 
-  const svg = d3
-    .select("#answers")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
-  // Wordcloud features that are different from one word to the other must be here
-  const layout = d3.layout
-    .cloud()
-    .size([width, height])
-    .words(answers.map((d) => ({ text: d.word, size: d.size })))
-    .padding(5)
-    .rotate(function () {
-      return ~~(Math.random() * 2) * 90;
-    })
-    .fontSize((d) => d.size)
-    .on("end", draw);
-  layout.start();
-
-  // This function takes the output of 'layout' above and draw the words
-  // Wordcloud features that are THE SAME from one word to the other can be here
-  function draw(words) {
-    svg
-      .append("g")
-      .attr(
-        "transform",
-        "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")"
-      )
-      .selectAll("text")
-      .data(words)
-      .enter()
-      .append("text")
-      .style("font-size", (d) => d.size)
-      .style("fill", "black")
-      .attr("text-anchor", "middle")
-      .style("font-family", "Impact")
-      .attr("transform", function (d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text((d) => d.text);
-  }
-};
-
-hooks.drawWordCloud = {
-  mounted() {
-    drawCloud(answers);
-  },
-};
-
-window.addEventListener("phx:add-answer", (e) => {
-  drawCloud(e.detail.answers.map((word) => ({ word, size: "20" })));
-});
+// window.addEventListener("phx:add-answer", (e) => {
+//   drawCloud(e.detail.answers.map((word) => ({ word, size: "20" })));
+// });
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
